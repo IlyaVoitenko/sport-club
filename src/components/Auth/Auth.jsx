38ab312f7a44da371ce23cdb401707b59da480cf
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2023-12-02 00:04:35 +0100

    added poster and header

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,67 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { isMessageErrorSelector } from "../../store/selectors";
 import { setIsMessageError, setIsAuth } from "../../store/reducers/auth";
+import { isAuthSelector } from "../../store/selectors";
+import { Button, Card, Label, TextInput } from "flowbite-react";
 
 const Auth = () => {
   const navigate = useNavigate();
   const [login, setLogin] = useState(null);
   const [password, setPassword] = useState(null);
-  const isMessageError = useSelector(isMessageErrorSelector);
-  //i.agarkov
-  //12345678
-  // useEffect(() => {
-  //   navigate("/list-transactions");
-  // }, [navigate]);
-
+  const isAuth = useSelector(isAuthSelector);
   const dispatch = useDispatch();
 
-  return (
-    <div className="flex items-center justify-center	bg-neutral-900  h-screen w-screen  align">
-      <form className="flex flex-col items-center justify-center f  h-3/6 w-[20%] space-y-4 md:space-y-6">
-        {isMessageError && (
-          <div className="flex flex-col items-center justify-center max-sm:w-48  bg-red-700 pl-2 pr-2 pt-2 pb-2">
-            <span className="text-white ">
-              Error: Incorrect login or password
-            </span>
-          </div>
-        )}
+  useEffect(() => {
+    console.log(isAuth);
+  }, [isAuth]);
 
-        <div>
-          <label
-            htmlFor="text"
-            className="block mb-2 text-sm  text-slate-400  text-left"
-          >
-            Login
-          </label>
-          <input
-            type="text"
-            name="text"
-            id="text"
-            className="outline-none border-b-[1px] border-blue-700 bg-neutral-900  text-white pt-1 pb-1 "
-            onChange={({ target }) => setLogin(target.value)}
-            required=""
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm text-slate-400 text-left"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className=" outline-none border-b-[1px] border-blue-700 bg-neutral-900  text-white pt-1 pb-1 "
-            onChange={({ target }) => setPassword(target.value)}
-            required=""
-          />
-        </div>
-        <div className="flex items-center justify-center w-[60%] max-sm:w-full  ">
-          <button
+  return (
+    <div className="w-screen flex justify-center">
+      <Card className="max-w-sm">
+        <form className="flex flex-col gap-4 bg-inherit">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email1" value="Your login" />
+            </div>
+            <TextInput
+              id="email1"
+              type="text"
+              placeholder="user"
+              onChange={({ target }) => setLogin(target.value)}
+              required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Your password" />
+            </div>
+            <TextInput
+              id="password1"
+              type="password"
+              placeholder="user"
+              onChange={({ target }) => setPassword(target.value)}
+              required
+            />
+          </div>
+          <Button
+            type="submit"
+            color="blue"
             onClick={(e) => {
               e.preventDefault();
               if (login === "user" && password === "user") {
@@ -71,12 +56,11 @@ const Auth = () => {
               }
               dispatch(setIsMessageError(true));
             }}
-            className=" bg-blue-700  pt-2 pb-2 text-white rounded-md "
           >
-            submit
-          </button>
-        </div>
-      </form>
+            Submit
+          </Button>
+        </form>
+      </Card>
     </div>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,91 @@
+import { NavLink } from "react-router-dom";
+import { setIsAuth } from "../../store/reducers/auth";
+import { useDispatch } from "react-redux";
+
+const Header = () => {
+  const dispatch = useDispatch();
+  return (
+    <header>
+      <nav className="fixed w-screen px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="https://flowbite.com" className="flex items-center">
+            <img
+              src="/logo.png"
+              className=" h-11  sm:h-9"
+              alt="Flowbite Logo"
+            />
+            {/* <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Sport Club
+            </span> */}
+          </a>
+          <div className="flex items-center lg:order-2">
+            <button
+              href="#"
+              onClick={() => dispatch(setIsAuth())}
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+            >
+              Log out{" "}
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <NavLink
+                  to={"/main"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                      : "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/Post"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                      : "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  }
+                >
+                  Post
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/blog"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                      : "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  }
+                >
+                  Blog
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/#"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                      : "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  }
+                >
+                  Company
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
