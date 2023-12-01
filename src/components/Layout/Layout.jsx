b3919f1f42a951bde7bdb979c96b95e67b461c63
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2023-12-02 00:28:23 +0100

    added loading page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import LoadingPage from "../../pages/LoadingPage";
 
 const Layout = () => {
   return (
     <>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<LoadingPage />}>
         <Outlet />
       </Suspense>
     </>
